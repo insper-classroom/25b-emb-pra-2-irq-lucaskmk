565d0dec291d4579600d5ee0d59e5748c5aa4c3c
LKenji <54600289+lucaskmk@users.noreply.github.com>
2025-08-20 20:23:17 -0300

    ex0

--- exe0/main.c
+++ exe0/main.c
@@ -2,10 +2,6 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
-
 const int BTN_PIN_R = 28;
 
 int btn_flag;
@@ -32,8 +28,7 @@ int main() {
   gpio_init(BTN_PIN_R);
   gpio_set_dir(BTN_PIN_R, GPIO_IN);
   gpio_pull_up(BTN_PIN_R);
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,&btn_callback);
 
   volatile int capture_flag = 0;
   int a;
